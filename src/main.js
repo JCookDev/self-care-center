c17eb94809cf0f62770241e93b02fa7ae97cffd6
Jeffrey Cook <cookturingedu@gmail.com>
2022-05-29 22:13:40 -0400

    Add access to html elements in main.js file

---
+++ src/main.js
@@ -0,0 +1,7 @@
+var affirmButton = document.querySelector(".affirmation");
+var mantraButton = document.querySelector(".mantra");
+var receive = document.querySelector(".receive-message");
+var messageArea = document.querySelector(".message-area");
+
+var meditateImg = document.querySelector(".meditate-img");
+var messages = document.querySelector(".message-display");
