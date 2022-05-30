cf8a7dcb6e821a967f0fba24c283887e63034dbd
Jeffrey Cook <cookturingedu@gmail.com>
2022-05-29 23:22:10 -0400

    Add event listener and event handler to diaplay messages

--- src/main.js
+++ src/main.js
@@ -5,3 +5,22 @@ var messageArea = document.querySelector(".message-area");
 
 var meditateImg = document.querySelector(".meditate-img");
 var messages = document.querySelector(".message-display");
+
+receive.addEventListener('click', displayMessage);
+
+function displayMessage() {
+  if (affirmButton.checked === true || mantraButton.checked === true){
+    meditateImg.hidden = true;
+    messages.hidden = false;
+    if(affirmButton.checked){
+      messages.innerText = affirmations[getRandomIndex(affirmations)];
+    } else if (mantraButton.checked){
+      messages.innerText = mantras[getRandomIndex(mantras)];
+    }
+  }
+}
+
+
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
+}
