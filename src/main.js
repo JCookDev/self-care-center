7e6dd5e4760065d7c8ce26d59fbf15519d6411c7
Jeffrey Cook <cookturingedu@gmail.com>
2022-05-29 23:50:28 -0400

    Add delete-message alert

--- src/main.js
+++ src/main.js
@@ -2,15 +2,18 @@ var affirmButton = document.querySelector(".affirmation");
 var mantraButton = document.querySelector(".mantra");
 var receive = document.querySelector(".receive-message");
 var messageArea = document.querySelector(".message-area");
+var deleteMssg = document.querySelector(".delete-message");
 
 var meditateImg = document.querySelector(".meditate-img");
 var messages = document.querySelector(".message-display");
 
 receive.addEventListener('click', displayMessage);
+deleteMssg.addEventListener('click', deleteMessage);
 
 function displayMessage() {
   if (affirmButton.checked === true || mantraButton.checked === true){
     meditateImg.hidden = true;
+    deleteMssg.hidden = false;
     messages.hidden = false;
     if(affirmButton.checked){
       messages.innerText = affirmations[getRandomIndex(affirmations)];
@@ -20,6 +23,12 @@ function displayMessage() {
   }
 }
 
+function deleteMessage(){
+  meditateImg.hidden = false;
+  deleteMssg.hidden = true;
+  messages.hidden = true;
+  alert("Message deleted.");
+}
 
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
